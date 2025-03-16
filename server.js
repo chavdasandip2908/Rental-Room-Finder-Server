6feae50d1e6631113e62a46f4e3f5b628b99bf6d
“Chavda <chavdasandip2908@gmail.com>
2025-03-16 16:03:15 +0530

    add user routers

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(express.json());
 
 connectDB();
 
-app.use("/api/auth", require("./src/routes/authRoutes"));
-app.use("/api/users", require("./src/routes/userRoutes"));
+app.use("/api/v1/auth", require("./src/routes/authRoutes"));
+app.use("/api/v1/users", require("./src/routes/userRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
