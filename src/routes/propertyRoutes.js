c7820b4992d8018e25eb95cc57695d280971752d
“Chavda <chavdasandip2908@gmail.com>
2025-03-19 11:31:31 +0530

    add the property left route

--- src/routes/propertyRoutes.js
+++ src/routes/propertyRoutes.js
@@ -8,6 +8,11 @@ const {
     getGalleryProperties,
     approveGallery,
     markPropertySold,
+    requestProperty,
+    getPropertyRequests,
+    approvePropertyRequest,
+    rejectPropertyRequest,
+    searchProperties,
 } = require("../controllers/propertyController");
 const { protect, isAdmin } = require("../middlewares/authMiddleware");
 
@@ -16,10 +21,16 @@ const router = express.Router();
 router.post("/create", protect, createProperty);
 router.get("/", getProperties);
 router.get("/:id", getPropertyById);
-router.put("/update/:id", protect, updateProperty); 
+router.put("/update/:id", protect, updateProperty);
 router.delete("/delete/:id", protect, deleteProperty);
 router.get("/gallery", getGalleryProperties);
 router.put("/gallery/approve/:id", protect, isAdmin, approveGallery);
-router.put("/sold/:id", protect, markPropertySold); 
+router.put("/sold/:id", protect, markPropertySold);
+router.post("/request/:pid", protect, requestProperty);
+router.get("/requests/:pid", protect, getPropertyRequests);
+router.put("/approve-request/:requestId", protect, approvePropertyRequest);
+router.put("/reject-request/:requestId", protect, rejectPropertyRequest);
+router.get("/search/properties", searchProperties);
+
 
 module.exports = router;
