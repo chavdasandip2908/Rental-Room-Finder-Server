6a10ef2c27230083a2c645f56cc3abc100c0d408
“Chavda <chavdasandip2908@gmail.com>
2025-03-19 22:18:53 +0530

    change the isadmin message and delete rolemiddleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -15,7 +15,7 @@ const protect = (req, res, next) => {
 
 const isAdmin = (req, res, next) => {
   if (req.user.role !== "admin") {
-    return res.status(403).json({ message: "Access Forbidden!" });
+    return res.status(403).json({ message: "Access denied. You are not authorized to perform this action." });
   }
   next();
 };
