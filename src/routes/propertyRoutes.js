cff968de211b65c0b0e220dd27902429297100fc
“Chavda <chavdasandip2908@gmail.com>
2025-03-19 22:07:53 +0530

    fix the crashing route error

--- src/routes/propertyRoutes.js
+++ src/routes/propertyRoutes.js
@@ -20,10 +20,10 @@ const router = express.Router();
 
 router.post("/create", protect, createProperty);
 router.get("/", getProperties);
-router.get("/:id", getPropertyById);
 router.put("/update/:id", protect, updateProperty);
 router.delete("/delete/:id", protect, deleteProperty);
 router.get("/gallery", getGalleryProperties);
+router.get("/:id", getPropertyById);
 router.put("/gallery/approve/:id", protect, isAdmin, approveGallery);
 router.put("/sold/:id", protect, markPropertySold);
 router.post("/request/:pid", protect, requestProperty);
