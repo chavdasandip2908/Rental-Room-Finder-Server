324d1922da21e9a225a279a7b5df4308e614fdbf
“Chavda <chavdasandip2908@gmail.com>
2025-03-26 16:40:21 +0530

    fix error

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./src/config/db");
-
-const express = require('express');
 const app = express();
 
 app.use(express.json({ limit: '50mb' }));
