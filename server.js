6f6fc5566c52310d946a1d2d523922a9ed1099a5
“Chavda <chavdasandip2908@gmail.com>
2025-03-26 16:27:13 +0530

    imcress size

--- server.js
+++ server.js
@@ -6,13 +6,8 @@ const connectDB = require("./src/config/db");
 const express = require('express');
 const app = express();
 
-// ✅ Body-parser limit increase karein
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
-// ✅ Multer middleware agar use kar rahe ho to:
-const multer = require('multer');
-const upload = multer({ limits: { fileSize: 50 * 1024 * 1024 } }); // 50MB limit
 app.use(cors());
 
 connectDB();
