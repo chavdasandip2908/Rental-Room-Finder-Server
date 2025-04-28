2783022f0734b99c5fcb9f031fb24eab40cca17b
“Chavda <chavdasandip2908@gmail.com>
2025-04-28 09:42:16 +0530

    add contact route path

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors({
     //     }
     // },
     origin: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
@@ -33,6 +33,7 @@ app.use("/api/v1/users", require("./src/routes/userRoutes"));
 app.use("/api/v1/properties", require("./src/routes/propertyRoutes"));
 app.use("/api/v1/blogs", require("./src/routes/blogRoutes"));
 app.use("/api/v1/payment", require("./src/routes/paymentRoutes"));
+app.use("/api/v1/contact", require("./src/routes/contactRoutes"));
 app.get("/test", (req, res) => res.send("success running test"))
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
