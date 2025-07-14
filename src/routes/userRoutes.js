5d4522a2d1b23a075cc2fed3e40ab48a0f3c02cf
“Chavda <chavdasandip2908@gmail.com>
2025-07-14 22:55:05 +0530

    add one middleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { getProfile, updateUser, deleteUser,getAllUser } = require("../controllers/userController");
-const { protect, isAdmin } = require("../middlewares/authMiddleware");
+const { getProfile, updateUser, deleteUser, getAllUser } = require("../controllers/userController");
+const { protect, isAdmin, isSelfOrAdmin } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
-router.get("/",protect, getAllUser);
-router.get("/profile", protect, getProfile);
-router.put("/update", protect, updateUser);
+router.get("/", protect, getAllUser);
+router.get("/profile", protect, isSelfOrAdmin, getProfile);
+router.put("/update", protect, isSelfOrAdmin, updateUser);
 router.delete("/delete/:id", protect, isAdmin, deleteUser);
 
 module.exports = router;
