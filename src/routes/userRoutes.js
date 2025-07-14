416bccce4735f3c54d1f24617060f25e067e76f5
chavdasandip2908 <112772240+chavdasandip2908@users.noreply.github.com>
2025-07-14 13:30:09 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { getProfile, updateUser, deleteUser } = require("../controllers/userController");
+const { getProfile, updateUser, deleteUser,getAllUser } = require("../controllers/userController");
 const { protect, isAdmin } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
-router.get("/", protect, getAllUser);
+router.get("/", getAllUser);
 router.get("/profile", protect, getProfile);
 router.put("/update", protect, updateUser);
 router.delete("/delete/:id", protect, isAdmin, deleteUser);
